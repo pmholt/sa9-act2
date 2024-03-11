5008b32da9287512d2c1c0b68ae567af3cec9453
Peyton Holt <pmholt@memphis.edu>
2024-03-11 13:44:46 -0500

    tried fixing rspec

--- spec/BankAccount_class_spec.rb
+++ spec/BankAccount_class_spec.rb
@@ -1,31 +1,31 @@
 RSpec.describe BankAccount do
-    describe "#deposit" do
-      it "increases the balance by the deposit amount" do
-        bank_account = BankAccount.new(10)
-        bank_account.deposit(50)
-        expect(bank_account.balance).to eq(60)
-      end
+  describe "#deposit" do
+    it "increases the balance by the deposit amount" do
+      bank_account = BankAccount.new(10)
+      bank_account.deposit(50)
+      expect(bank_account.balance).to eq(60)
+    end
+  end
+  
+  describe "#withdraw" do
+    it "decreases the balance by the withdrawal amount if funds are available" do
+      bank_account = BankAccount.new(100)
+      bank_account.withdraw(10)
+      expect(bank_account.balance).to eq(90)
     end
   
-    describe "#withdraw" do
-      it "decreases the balance by the withdrawal amount if funds are available" do
-        bank_account = BankAccount.new(100)
-        bank_account.withdraw(10)
-        expect(bank_account.balance).to eq(90)
-      end
-  
-      it "does not change the balance if insufficient funds" do
-        bank_account = BankAccount.new(100)
-        bank_account.withdraw(101)
-        expect(bank_account.balance).to eq(100)
-      end
+    it "does not change the balance if insufficient funds" do
+      bank_account = BankAccount.new(100)
+      bank_account.withdraw(101)
+      expect(bank_account.balance).to eq(100)
     end
+  end
   
-    describe "#balance" do
-      it "returns the current balance" do
-        bank_account = BankAccount.new(100)
-        expect(bank_account.balance).to eq(100)
-      end
+  describe "#balance" do
+    it "returns the current balance" do
+      bank_account = BankAccount.new(100)
+      expect(bank_account.balance).to eq(100)
     end
+  end
 end
   
